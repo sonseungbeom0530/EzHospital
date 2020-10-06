95eae5fe239f1a302edc11db1490fb335ec777b3
sonseungbeom0530 <ssb05300@naver.com>
2020-10-06 16:10:28 +0800

    Create Home screen

--- app/src/main/java/com/example/ezhospital/MainActivity.java
+++ app/src/main/java/com/example/ezhospital/MainActivity.java
@@ -1,18 +1,61 @@
 package com.example.ezhospital;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
+import com.example.ezhospital.Fragment.HomeFragment;
+import com.example.ezhospital.Fragment.ShoppingFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.bottomsheet.BottomSheetDialog;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import dmax.dialog.SpotsDialog;
+
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.bottom_navigation)
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ButterKnife.bind(MainActivity.this);
+
+        //view
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            Fragment fragment=null;
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                if (menuItem.getItemId()==R.id.action_home)
+                    fragment=new HomeFragment();
+                else if (menuItem.getItemId()==R.id.action_shopping)
+                    fragment=new ShoppingFragment();
+
+                return loadFragment(fragment);
+            }
+        });
+        bottomNavigationView.setSelectedItemId(R.id.action_home);
+    }
+
+    private boolean loadFragment(Fragment fragment) {
+        if (fragment!=null){
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,fragment)
+                    .commit();
+            return true;
+        }
+        return false;
     }
 }
